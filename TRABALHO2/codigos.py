8e4d1714384c096372e0dccfaa19172cf2ecb5c5
SamuelCarvalhoC <samuelcarvalho126@gmail.com>
2026-04-29 23:20:51 -0300

    Update codigos.py

--- TRABALHO2/codigos.py
+++ TRABALHO2/codigos.py
@@ -1,1 +1,21 @@
+def heapify(arr, n, i):
+    maior = i 
+    esquerda = 2 * i + 1
+    direita = 2 * i + 2
 
+    if esquerda < n and arr[i] < arr[esquerda]:
+        maior = esquerda
+
+    if direita < n and arr[maior] < arr[direita]:
+        maior = direita
+
+    if maior != i:
+        arr[i], arr[maior] = arr[maior], arr[i]
+        heapify(arr, n, maior)
+
+def build_heap(arr):
+    n = len(arr)
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+  
